a3b6001e2152f093f1194dc58b3897f579f0bcb0
Ibrahim <ibrahimmahamudaaa@gmail.com>
2019-11-24 11:50:44 +0300

    Add Earthquake search activity and make it main

---
+++ app/src/main/java/com/example/earthquake/EarthquakeSearchActivity.java
@@ -0,0 +1,12 @@
+package com.example.earthquake;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EarthquakeSearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_earthquake_detailed);
+    }
+}
